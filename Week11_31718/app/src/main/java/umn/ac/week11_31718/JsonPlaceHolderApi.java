0fd86afddf6788d6369451616ed610c3869b75af
Hnsenchristiann <christiansensen00@gmail.com>
2021-04-26 10:44:35 +0700

    Retrofit

---
+++ Week11_31718/app/src/main/java/umn/ac/week11_31718/JsonPlaceHolderApi.java
@@ -0,0 +1,12 @@
+package umn.ac.week11_31718;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
